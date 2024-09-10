bc1b84dc96671796f3418694868037543f39af32
pipeb <pipeba1408@gmail.com>
2024-09-10 07:28:18 -0500

    Initial commit

---
+++ src/main/java/com/arquitectura/aerolineaback/AerolineaBackApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura.aerolineaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AerolineaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AerolineaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arquitectura/aerolineaback/AerolineaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura.aerolineaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AerolineaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
