502b9ed33277b77073ee27d86da355ed2921a3b5
pipeb <pipeba1408@gmail.com>
2024-09-13 21:09:57 -0500

    Elimina método innecesario

--- src/main/java/com/arquitectura/aerolineaback/persistencia/jpa/PersonaJPA.java
+++ src/main/java/com/arquitectura/aerolineaback/persistencia/jpa/PersonaJPA.java
@@ -4,9 +4,7 @@ import com.arquitectura.aerolineaback.persistencia.orm.PersonaORM;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PersonaJPA extends JpaRepository<PersonaORM, String> {
-    List<PersonaORM> findByPassportId(String passportId);
+
 }
