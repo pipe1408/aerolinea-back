1bda02b15619cf7f4cc57c262fae8ddd0f1b9017
pipeb <pipeba1408@gmail.com>
2024-09-29 13:20:48 -0500

    Corrige

--- src/main/java/com/arquitectura/aerolineaback/AerolineaBackApplication.java
+++ src/main/java/com/arquitectura/aerolineaback/AerolineaBackApplication.java
@@ -1,4 +1,4 @@
-package com.arquitectura.aerolineabak;
+package com.arquitectura.aerolineaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
