df646ce43221f2e14934d4c137458e4c91a2a29e
pipeb <pipeba1408@gmail.com>
2024-10-28 06:11:10 -0500

    correct bad commit

--- src/main/java/com/arquitectura/aerolineaback/AerolineaBackApplication.java
+++ src/main/java/com/arquitectura/aerolineaback/AerolineaBackApplication.java
@@ -1,4 +1,4 @@
-paackage com.arquitectura.aerolineaback;
+package com.arquitectura.aerolineaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
