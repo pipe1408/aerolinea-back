bac1894dcf3d3040fd8e117228ab0bc595e9bf9e
pipeb <pipeba1408@gmail.com>
2024-11-22 01:59:05 -0500

    Add tests for new classes

---
+++ src/test/java/com/arquitectura/aerolineaback/rabbit/RabbitConfigTest.java
@@ -0,0 +1,33 @@
+package com.arquitectura.aerolineaback.rabbit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.Binding;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class RabbitConfigTest {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void testRabbitConfigBeans() {
+        DirectExchange exchange = applicationContext.getBean(DirectExchange.class);
+        Queue queue = applicationContext.getBean(Queue.class);
+        Binding binding = applicationContext.getBean(Binding.class);
+
+        assertNotNull(exchange);
+        assertNotNull(queue);
+        assertNotNull(binding);
+
+        assertEquals(RabbitConfig.EXCHANGE_NAME, exchange.getName());
+        assertEquals(RabbitConfig.QUEUE_NAME, queue.getName());
+        assertEquals(RabbitConfig.ROUTING_KEY, binding.getRoutingKey());
+    }
+}
